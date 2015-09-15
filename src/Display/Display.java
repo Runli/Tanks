3a4f76d2d523e656f691345cdd158e2bb7666520
ILNUR Gazizov <ilnur.gazizov@phystech.edu>
2015-09-16 01:15:00 +0300

    delete static render

--- src/Display/Display.java
+++ src/Display/Display.java
@@ -19,16 +19,10 @@ public abstract class Display {
 
         window = new JFrame(title);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        content = new Canvas(){
-            public void paint(Graphics g){
-                super.paint(g);
-                render(g); //
-            }
-        };
+        content = new Canvas();
 
         Dimension size = new Dimension(width, height);
         content.setPreferredSize(size); //лист имеет размер width and height
-        content.setBackground(Color.black);
 
         window.setResizable(false); //нельзя менять размер окна
         window.getContentPane().add(content);
@@ -37,14 +31,4 @@ public abstract class Display {
         window.setVisible(true);
     }
 
-    public static void render(){
-        content.repaint();
-    }
-
-    private static void render(Graphics g){
-
-        g.setColor(Color.white);
-        g.fillOval(400 - 50, 300 - 50, 100, 100);
-
-    }
 }
