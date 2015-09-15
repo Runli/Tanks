d2ba2511d3038a492c8a0e76196cc63f17277d78
ILNUR Gazizov <ilnur.gazizov@phystech.edu>
2015-09-16 00:26:15 +0300

    Battle City on Java was created

---
+++ src/Display/Display.java
@@ -0,0 +1,33 @@
+package Display;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by ilnurgazizov on 16.09.15.
+ */
+public abstract class Display {
+
+    private static boolean created = false; // Проверка создания окна
+    private static JFrame window;
+    private static Canvas content; // Лист рисования
+
+    public static void create(int width, int height, String title){
+
+        //Проверка
+        if (created) return;
+
+        window = new JFrame(title);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        content = new Canvas();
+
+        Dimension size = new Dimension(width, height);
+        content.setPreferredSize(size); //лист имеет размер width and height
+
+        window.setResizable(false); //нельзя менять размер окна
+        window.getContentPane().add(content);
+        window.pack(); // контент будет находится внутри окна без потерь
+        window.setLocationRelativeTo(null); // меняем позицию окна в зависимости от переданного компонента
+        window.setVisible(true);
+    }
+}
---
+++ src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+import Display.Display;
+/**
+ * Created by ilnurgazizov on 16.09.15.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Display.create(800, 600, "Tanks");
+
+    }
+}
